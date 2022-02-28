c9f40ed73a581530c2653799e7d1e1df801ddc4b
Sergio Ricardo Carlos <sergiorc@gmail.com>
2022-02-28 11:49:17 -0300

    Change the welcome message

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World!"
+    return "Hello this is the new version!"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
